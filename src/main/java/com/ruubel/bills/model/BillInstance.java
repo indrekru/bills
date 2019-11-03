7958b3786e02dbf8bdd6592009dfec32ae23b602
Indrek Ruubel <indrekruubel@gmail.com>
2019-11-03 12:35:14 +0200

    dev

--- src/main/java/com/ruubel/bills/model/BillInstance.java
+++ src/main/java/com/ruubel/bills/model/BillInstance.java
@@ -21,9 +21,16 @@ public class BillInstance {
     @Column(name = "price")
     private Double price;
 
+    @Column(name = "paid")
+    private boolean paid;
+
     @ManyToOne
     @JoinColumn(name = "bill")
     private Bill bill;
+
+    @Column(name = "paid_at")
+    @Convert(converter = TimestampConverter.class)
+    private Instant paidAt;
 
     @Column(name = "created_at")
     @Convert(converter = TimestampConverter.class)
@@ -35,7 +42,9 @@ public class BillInstance {
     public BillInstance(Double price, String externalId, Bill bill) {
         this.price = price;
         this.externalId = externalId;
+        this.paid = false;
         this.bill = bill;
+        this.paidAt = null;
         this.createdAt = Instant.now();
     }
 
